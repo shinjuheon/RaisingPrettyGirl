99ed763e1402e9bc0c7caab71fad6fc4c48c041d
김주영 <gunggme@amuz.co.kr>
2026-01-10 18:45:24 +0900

    one store sdk added

--- Assets/01. Scripts/Rank/Constants.cs
+++ Assets/01. Scripts/Rank/Constants.cs
@@ -1,6 +1,9 @@
-public class Constants
+namespace gunggme
 {
-    public static readonly string USER_DATA_TABLE = "PlayerLevelStatData";
-    public static readonly int MAX_RANK_LIST = 10;
-    public static readonly string DAILY_RANK_UUID = "aad59260-0dc0-11ef-a648-af0267cadfc5"; //뒤끝에서 랭킹을 생성하면 나오는 UUID
+    public class Constants
+    {
+        public static readonly string USER_DATA_TABLE = "PlayerLevelStatData";
+        public static readonly int MAX_RANK_LIST = 10;
+        public static readonly string DAILY_RANK_UUID = "aad59260-0dc0-11ef-a648-af0267cadfc5"; //뒤끝에서 랭킹을 생성하면 나오는 UUID
+    }
 }
